aa4e420a30c612215ce93a7df6bb2893d64898e8
Carlos Valderrama <cvalderrama1991@gmail.com>
2024-12-11 23:36:12 -0500

    added an html file and working on correcting the filename and dirname

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import http from 'node:http';
 import fs from 'fs/promises';
-import url from 'url';
+// import url from 'url';
 import path from 'path';
 
 const PORT = process.env.PORT;
 
 // Get current path
-const __filename = url.fileURLToPath(import.meta.url);
+const __filename = import.meta.filename;
+// const __dirname = import.meta.dirname;
 const __dirname = path.dirname(__filename);
 
 const server = http.createServer(function(req, res) {
@@ -30,6 +31,9 @@ const server = http.createServer(function(req, res) {
   }
 
 });
+
+console.log(__dirname);
+
 
 server.listen(PORT, function() {
   console.log(`Server is running on port: ${PORT}`);
